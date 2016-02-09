dee632870c1831be42052516fef80ca24e5ca0b3
maglet <tobin.magle@ucdenver.edu>
2016-02-09 11:08:39 -0700

    more progress
    added more screen grabs, fleshing out tutorial

--- r-for-beginners.R
+++ r-for-beginners.R
@@ -2,5 +2,11 @@
 x<-4 #assigns the value 4 to the variable x
 x #print x to the output screen
 
+#downloading files
+#     url = web location of the file
+#     destfile = desired file path and name. Defaults to working directory
+download.file(url="https://github.com/maglet/r-for-beginners/blob/master/inflammation.csv",
+              destfile = "inflammation.csv")
+
 
 
