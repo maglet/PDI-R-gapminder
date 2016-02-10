25ba3c56ec2650e0cbe9e7a90fe60a25d48c3749
maglet <tobin.magle@ucdenver.edu>
2016-02-10 14:48:47 -0700

    Tutorial V1 finished!

--- plotcode.R
+++ plotcode.R
@@ -1,28 +1,5 @@
 
-error.bar <- function(x, y, upper, lower=upper, length=0.1,...){
-     if(length(x) != length(y) | length(y) !=length(lower) | length(lower) != length(upper))
-          stop("vectors must be same length")
-     arrows(x,y+upper, x, y-lower, angle=90, code=3, length=length, ...)
-}
 
-```{r}
-barplot(dragons$TalonLength, names = dragons$Population, 
-        ylim=c(0,70),xlim=c(0,4),yaxs='i', xaxs='i',
-        main="Dragon Talon Length in the UK",
-        ylab="Mean Talon Length",
-        xlab="Country")
-par(new=T)
-plotCI (dragons$TalonLength, 
-        uiw = dragons$SE, liw = dragons$SE,
-        gap=0,sfrac=0.01,pch="",
-        ylim=c(0,70),
-        xlim=c(0.4,3.7),
-        yaxs='i', xaxs='i',axes=F,ylab="",xlab="")
-```
-
-```{r}
-boxplot(iris$Sepal.Length ~ iris$Species)
-```
 
 ```{r}
 plot(moomins$Year, moomins$PopSize,                              # x variable, y variable
@@ -38,7 +15,4 @@ abline(fit1, lty = "dashed")                            # add the regression lin
 text(x = 1978, y = 750, labels = "R2 = 0.896\nP = 2.615e-15")   # add a label to the plot at (x,y)
 ```
 
-```{r}
-pairs(iris, col = iris$Species)
-```
 
